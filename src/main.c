edec9911c717fb216ffe1cb2a37de88b9d64d7bf
lalancz <lalancz77@gmail.com>
2023-06-05 01:00:55 +0200

    hackify welcome message, add compile command hint

--- src/main.c
+++ src/main.c
@@ -1,17 +1,49 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
 #define MAX_LEN 100
 #define MAX_HISTORY_LEN 10
 
+// compile command needs -lreadline flag (gcc main.c -lreadline)
+
 char history[MAX_HISTORY_LEN][MAX_LEN];
 char *input_buffer;
 
+void print_welcome_message() {
+	char welcome_message[] = "Welcome to prosh [Version 0.01]";
+	char c;
+	int compare;
+	int index = 0;
+
+	srand(time(NULL));
+
+	while (1) {
+		c = "qwertyuiopasdfghjklzxcvbnmWelcome to prosh [Version 0.01]"[rand() % 57];
+		printf("%c", c);
+
+		if (c == welcome_message[index]) {
+			index++;
+		} else {
+			usleep(2500);
+			printf("\b");
+			fflush(stdout);
+		}
+
+		if (index == 31) {
+			printf("\n");
+			return;
+		}
+	}
+}
+
 int main()
 {
-	printf("prosh [Version 0.01]\n");
+	print_welcome_message();
 
 	using_history();
 	stifle_history(MAX_HISTORY_LEN);
