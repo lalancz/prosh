2fc7999bc5f23be70ea4629241eb59893d566415
lalancz <lalancz77@gmail.com>
2023-06-06 17:33:45 +0200

    add command parsing, change directory functionality

--- src/main.c
+++ src/main.c
@@ -41,6 +41,35 @@ void print_welcome_message() {
 	}
 }
 
+int get_command_id(char* command_string) {
+	char *command = strtok(command_string, " ");
+
+	if (strcmp(command, "cd") == 0) {
+		return 1;
+	} else if (strcmp(command, "ls") == 0) {
+		return 2;
+	}
+}
+
+void change_directory(char* argument) {
+	if (argument == NULL) {
+		return;
+	} else {
+		int result_code = chdir(argument);
+
+		if (result_code != 0)
+			printf("Could not change directory to %s\n", argument);
+	}
+}
+
+void list_directory() {
+
+}
+
+void execute_file() {
+
+}
+
 int main()
 {
 	print_welcome_message();
@@ -48,11 +77,14 @@ int main()
 	using_history();
 	stifle_history(MAX_HISTORY_LEN);
 
+	int command_id;
+	char* argument;
+
 	char cwd[MAX_LEN];
-	getcwd(cwd, MAX_LEN);
 
 	while (1)
 	{
+		getcwd(cwd, MAX_LEN);
 		printf("%s", cwd);
 
 		input_buffer = readline(">");
@@ -60,6 +92,20 @@ int main()
 		if (strlen(input_buffer) != 0)
 		{
 			add_history(input_buffer);
+
+			command_id = get_command_id(input_buffer);
+
+			switch (command_id) {
+				case 1:
+					argument = strtok(NULL, " ");
+					change_directory(argument);
+					break;
+				case 2:
+
+				default:
+
+
+			}
 		}
 	}
 }
